0ad2503e8a0fcceaca37d3347f84b7e5994e03f2
GrandguyJS <GrandguyMC@gmail.com>
2024-08-10 22:54:28 +0200

    added config menu

---
+++ Jellyfin.Plugin.RemoteUpload/Plugin.cs
@@ -0,0 +1,48 @@
+using System;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.RemoteUpload;
+
+/// <summary>
+/// Represents the RemoteUpload plugin for Jellyfin.
+/// </summary>
+public class Plugin : BasePlugin<PluginConfiguration>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Plugin"/> class.
+    /// </summary>
+    /// <param name="applicationPaths">The application paths.</param>
+    /// <param name="xmlSerializer">The XML serializer.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+
+    /// <summary>
+    /// Gets the name of the plugin.
+    /// </summary>
+    public override string Name => "RemoteUpload";
+
+    /// <summary>
+    /// Gets the unique identifier for the plugin.
+    /// </summary>
+    public override Guid Id => Guid.Parse("b5a2d6b3-c5f4-4f88-aed4-5f3e9877d0a6");
+
+    /// <summary>
+    /// Gets the description of the plugin.
+    /// </summary>
+    public override string Description => "A sample plugin for Jellyfin.";
+
+    /// <summary>
+    /// Gets the plugin instance.
+    /// </summary>
+    public static Plugin? Instance { get; private set; }
+
+    /// <summary>
+    /// Gets the plugin configuration.
+    /// </summary>
+    public PluginConfiguration PluginConfiguration => Configuration;
+}
---
+++ Jellyfin.Plugin.RemoteUpload/PluginConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.RemoteUpload;
+
+/// <summary>
+/// Represents the configuration settings for the RemoteUpload plugin.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    // Define properties for plugin configuration if needed
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        // Initialize properties with default values if necessary
+    }
+}
