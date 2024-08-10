7aecc5e60c44f13a3e4a2f6439fad5e23b45afd9
GrandguyJS <GrandguyMC@gmail.com>
2024-08-10 17:38:07 +0200

    first2

---
+++ Jellyfin.Plugin.MyPlugin/PluginConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.MyPlugin;
+
+/// <summary>
+/// Represents the configuration settings for the MyPlugin plugin.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    // Define properties for plugin configuration if needed
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        // Initialize properties with default values if necessary
+    }
+}
